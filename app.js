25fde6549c2adb9ad42f8ff6db5f0dc0e09759ac
A.K <akcoding2020@gmail.com>
2021-10-09 22:51:22 -0400

    work on education page

---
+++ app.js
@@ -0,0 +1,17 @@
+// When we are done loading
+window.addEventListener("load", function () {
+
+
+    // Deal with the navbar
+    const link = window.location.href
+    console.log(link)
+
+
+
+
+
+
+
+
+
+})
